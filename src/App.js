3ec804d9683ded782b5013b8196baee5ea9e5a43
KowshikShikder <www.kowshikder7@gmail.com>
2021-07-14 22:40:06 +0600

    Solving Loading Problem

--- src/App.js
+++ src/App.js
@@ -1,42 +1,16 @@
-import { lazy, Suspense } from 'react';
 import './App.css';
 
-
-import Loading from './Component/Loading/Loading';
+import Cover from './Component/Cover/Cover';
+import Navigation from './Component/Navigation/Navigation';
+import AboutSection from './Component/About/AboutSection';
+import MySkills from './Component/MySkills/MySkills';
+import Projects from './Component/Projects/Projects';
+import ClientsComment from './Component/ClientsComment/ClientsComment';
+import Services from './Component/Services/Services'
+import Contact from './Component/Contact/Contact'
+import Map from "./Component/Map/Map"
+import Footer from './Component/Footer/Footer';
 import {  HashRouter } from 'react-router-dom';
-
-
-
-// import Cover from './Component/Cover/Cover';
-const Cover = lazy(()=> import('./Component/Cover/Cover'))
-
-// import Navigation from './Component/Navigation/Navigation';
-const Navigation = lazy(()=> import('./Component/Navigation/Navigation'))
-
-// import AboutSection from './Component/About/AboutSection';
-const AboutSection = lazy(()=> import('./Component/About/AboutSection'))
-
-// import MySkills from './Component/MySkills/MySkills';
-const MySkills = lazy(()=> import('./Component/MySkills/MySkills'))
-
-// import Projects from './Component/Projects/Projects';
-const Projects = lazy(()=> import('./Component/Projects/Projects'))
-
-// import ClientsComment from './Component/ClientsComment/ClientsComment';
-const ClientsComment = lazy(()=> import('./Component/ClientsComment/ClientsComment'))
-
-// import Services from './Component/Services/Services'
-const Services = lazy(()=> import('./Component/Services/Services'))
-
-// import Contact from './Component/Contact/Contact'
-const Contact = lazy(()=> import('./Component/Contact/Contact'))
-
-// import Map from "./Component/Map/Map"
-const Map = lazy(()=> import("./Component/Map/Map"))
-
-// import Footer from './Component/Footer/Footer';
-const Footer = lazy(()=> import('./Component/Footer/Footer'))
-
 
 
 
@@ -44,7 +18,6 @@ const Footer = lazy(()=> import('./Component/Footer/Footer'))
 
 function App() {
   return (
-    <Suspense fallback={ <Loading />  }>
     <div className="App"> 
     
       <Cover />
@@ -62,10 +35,7 @@ function App() {
       <Map/>
       <Footer/>
 
-    
     </div>
-
-    </Suspense>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom';
+import Loading from './Component/Loading/Loading';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+const App = lazy(()=> import('./App'))
+
+
 
 ReactDOM.render(
   <React.StrictMode>
+  <Suspense fallback={ <Loading />  }>
+    
     <App />
+
+    </Suspense>
   </React.StrictMode>,
   document.getElementById('root')
 );
