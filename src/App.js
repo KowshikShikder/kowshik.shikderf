32104cbb3ca6e1056eb577024fe5c53a05bd9c29
KowshikShikder <www.kowshikder7@gmail.com>
2021-07-14 20:35:16 +0600

    Loading problem solved

--- src/App.js
+++ src/App.js
@@ -1,39 +1,52 @@
-
+import { lazy, Suspense } from 'react';
 import './App.css';
-import Cover from './Component/Cover/Cover';
-import Navigation from './Component/Navigation/Navigation';
-import AboutSection from './Component/About/AboutSection';
-import MySkills from './Component/MySkills/MySkills';
-import Projects from './Component/Projects/Projects';
-import ClientsComment from './Component/ClientsComment/ClientsComment';
-import Services from './Component/Services/Services'
-import Contact from './Component/Contact/Contact'
-import Map from "./Component/Map/Map"
-import Footer from './Component/Footer/Footer';
-import {  HashRouter } from 'react-router-dom';
+
+
 import Loading from './Component/Loading/Loading';
+import {  HashRouter } from 'react-router-dom';
+
+
+
+// import Cover from './Component/Cover/Cover';
+const Cover = lazy(()=> import('./Component/Cover/Cover'))
+
+// import Navigation from './Component/Navigation/Navigation';
+const Navigation = lazy(()=> import('./Component/Navigation/Navigation'))
+
+// import AboutSection from './Component/About/AboutSection';
+const AboutSection = lazy(()=> import('./Component/About/AboutSection'))
+
+// import MySkills from './Component/MySkills/MySkills';
+const MySkills = lazy(()=> import('./Component/MySkills/MySkills'))
+
+// import Projects from './Component/Projects/Projects';
+const Projects = lazy(()=> import('./Component/Projects/Projects'))
+
+// import ClientsComment from './Component/ClientsComment/ClientsComment';
+const ClientsComment = lazy(()=> import('./Component/ClientsComment/ClientsComment'))
+
+// import Services from './Component/Services/Services'
+const Services = lazy(()=> import('./Component/Services/Services'))
+
+// import Contact from './Component/Contact/Contact'
+const Contact = lazy(()=> import('./Component/Contact/Contact'))
+
+// import Map from "./Component/Map/Map"
+const Map = lazy(()=> import("./Component/Map/Map"))
+
+// import Footer from './Component/Footer/Footer';
+const Footer = lazy(()=> import('./Component/Footer/Footer'))
+
+
 
 
 
 
 function App() {
-
-
-  const StopLoading=()=>{
-    document.getElementById('loading').style.display="none"
-  }
-
-
-
-
-
-
-
-
   return (
-    <div className="App" onLoad={StopLoading}> 
-      <div id="loading" >  <Loading/> </div> 
-
+    <Suspense fallback={ <Loading />  }>
+    <div className="App"> 
+    
       <Cover />
       
       <HashRouter>
@@ -42,17 +55,17 @@ function App() {
 
       <AboutSection/>
       <MySkills />
-      <Projects id="project"/>
+      <Projects/>
       <ClientsComment />
       <Services/>
       <Contact/>
       <Map/>
       <Footer/>
 
-      
-
-
+    
     </div>
+
+    </Suspense>
   );
 }
 
