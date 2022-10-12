ce7bd862424156962b09a9553de0eed0a870a531
KowshikShikder <www.kowshikder7@gmail.com>
2022-10-12 16:43:19 +0600

    Update

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -15,19 +15,19 @@ export default function About() {
     return (
         <div  data-aos="about-animation" className="about-parent">
             <div className="About">
-                <p> Who am I
+                <p> Who I am
                     <div class="para1-style">
                         
                     </div>
                 </p>
                 <p className="about-description">
                     
-                        Working as a full-stack web developer for about 1 year. I have    created several websites specially for school , organization, online store.
+                        Working as a frontend web developer since 2020. I have created several websites specially for schools, organizations, online stores.
                     
                     <br/><br/>
-                        I use reactJS for frontend with HTML, CSS and JavaScript, NodeJS, expressJS for backend, Mongodb for database.
+                        Along with HTML and CSS I am quite good at react JS, next JS, Redux and obviously bootstrap & sass.
                 <br/><br/>
-                        Have the quality of Hard-working, quick-learning.
+                        I Have the quality of Hard-working, quick-learning.
                 </p>
 
                 <MyImage/>
